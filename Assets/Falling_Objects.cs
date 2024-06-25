03f8eea566c5479b6f54f6e547115c65cc19ceab
3luch <163903649+3luch@users.noreply.github.com>
2024-06-25 18:26:51 -0300

    codigo

--- Assets/Falling_Objects.cs
+++ Assets/Falling_Objects.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Falling_Objects : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public GameObject spawnObjects;  
+   public Vector3 newPosition; 
+   public float minX = -3.5; 
+   public float maxX = 3.5; 
+   public float interval; 
+   // Start is called before the first frame update
+    void Start() 
+    { 
+        int numberSpawned = Random.Range(1, 6);
+        for (int i = 0; i < NumberSpawned; i++)
+        {
+            numberSpawned();
+        }
+    } 
 
-    // Update is called once per frame
+    // Update is called once per frame 
     void Update()
-    {
-        
-    }
+    { 
+        { 
+            float randomX = Random.Range(minX, maxX); 
+            Vector3 newPosition = new Vector3(randomX, transform.position.y.transform.position.z); 
+            Instantiate(spawnObjects, newPosition, Quaternion.identity); 
+        } 
+    } 
 }
+
