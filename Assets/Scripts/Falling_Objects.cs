297a2881e31257f335f69a00eddcbd9d4fa74e41
3luch <163903649+3luch@users.noreply.github.com>
2024-06-26 19:19:30 -0300

    terminado Elian Engelberg

---
+++ Assets/Scripts/Falling_Objects.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Falling_Objects : MonoBehaviour
+{
+    public GameObject[] objectSpawn; 
+    public float minX = -3.5f;
+    public float maxX = 3.5f;
+    public float spawnY = 4f; 
+    public float spawnZ = -2.5f;
+
+    void Start()
+    {
+        SpawnObjects();
+    }
+
+    void SpawnObjects()
+    {
+        foreach (GameObject obj in objectSpawn)
+        {
+            int num = Random.Range(0, 2); 
+            for (int i = 0; i < num; i++)
+            {
+                float randomX = Random.Range(minX, maxX);
+                Vector3 newPosition = new Vector3(randomX, spawnY, spawnZ);
+                Instantiate(obj, newPosition, Quaternion.identity);
+            }
+        }
+    }
+}
