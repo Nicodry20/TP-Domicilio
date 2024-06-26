10327e3b3687b341e0949e05527560c1aa3013f5
3luch <163903649+3luch@users.noreply.github.com>
2024-06-25 22:14:38 -0300

    Completa la Parte de Elian Engelberg

--- Assets/Falling_Objects.cs
+++ Assets/Falling_Objects.cs
@@ -7,6 +7,8 @@ using UnityEngine.UI;
 public class Falling_Objects : MonoBehaviour
 {
    public GameObject martillo;  
+   public GameObject sillon; 
+   public GameObject mesa;   
    public float minX = -3.5f; 
    public float maxX = 3.5f; 
    public float Y = 4f; 
@@ -22,12 +24,27 @@ public class Falling_Objects : MonoBehaviour
     void SpawnObjects()
 
     { 
-        int numberSpawned = Random.Range(1, 7);
+        int numberSpawned = Random.Range(1, 3);
         for (int i = 0; i < numberSpawned; i++)
         { 
             float randomX = Random.Range(minX, maxX); 
             Vector3 newPosition = new Vector3(randomX, transform.position.y, transform.position.z);
             Instantiate(martillo, newPosition, Quaternion.identity); 
         } 
+
+        int numberSpawn = Random.Range(1, 3);
+        for (int h = 0; h < numberSpawn; h++)
+        { 
+            float randomX = Random.Range(minX, maxX); 
+            Vector3 newPosition = new Vector3(randomX, transform.position.y, transform.position.z);
+            Instantiate(sillon, newPosition, Quaternion.identity); 
+        }
+        int numberSpawnedd = Random.Range(1, 3);
+        for (int h = 0; h < numberSpawnedd; h++)
+        { 
+            float randomX = Random.Range(minX, maxX); 
+            Vector3 newPosition = new Vector3(randomX, transform.position.y, transform.position.z);
+            Instantiate(mesa, newPosition, Quaternion.identity); 
+        }
     } 
 }
